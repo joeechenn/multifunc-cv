e20726380d6f0b6e9d313a805e1b5fcf9aa03ac5
Joe Chen <jchen200611@gmail.com>
2025-09-17 12:52:30 -0400

    Draw landmark on detected hands and recognize common hand gestures

--- draw.py
+++ draw.py
@@ -4,6 +4,8 @@ TEXT_SPACING = 10
 FONT_SIZE = 1
 FONT_THICKNESS = 2
 
+RADIUS = 3
+
 def draw_face(img, faces, color=(0, 0, 225)):
     for f in faces:
         x,y,w,h = f['bbox']
@@ -24,4 +26,18 @@ def draw_face(img, faces, color=(0, 0, 225)):
                         cv2.FONT_HERSHEY_PLAIN,
                         FONT_SIZE,
                         color,
+                        FONT_THICKNESS)
+
+def draw_hands(img, hands, color=(255,0,0)):
+    for h in hands:
+        pts = h['landmarks']
+        for p in pts:
+            cv2.circle(img, p, RADIUS, color, -1)
+        if h["gesture"]:
+            x0, y0 = pts[0]
+            cv2.putText(img,
+                        f"{h['gesture']} ({h['score']:.2f})",
+                        (x0, y0 - TEXT_SPACING),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        FONT_SIZE, color,
                         FONT_THICKNESS)
